e541f35865d035f242043fef0ee157b1698b71c2
JANG-MINYOUNG <wkdalsdudqq@gmail.com>
2024-01-30 10:21:32 +0900

    회원가입 기능 구현, 회원 테이블 추가

--- DB.sql
+++ DB.sql
@@ -11,6 +11,17 @@ CREATE TABLE article(
     `body` TEXT NOT NULL
 );
 
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(100) NOT NULL,
+    loginPw CHAR(200) NOT NULL,
+    `name` CHAR(100) NOT NULL,
+    nickname CHAR(100) NOT NULL,
+    cellphoneNum CHAR(100) NOT NULL,
+    email CHAR(100) NOT NULL
+);
 # article TD 생성
 INSERT INTO article
 SET regDate = NOW(),
@@ -30,5 +41,29 @@ updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
 
+# Member TD 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = 'test1',
+nickname = 'test1',
+cellphoneNum = '010-0000-0000',
+email = 'test1@naver.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = 'test2',
+nickname = 'test2',
+cellphoneNum = '010-1111-0000',
+email = 'test2@naver.com';
+
 SELECT *
 FROM article;
+
+SELECT *
+FROM `member`;
