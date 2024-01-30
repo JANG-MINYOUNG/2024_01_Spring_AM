2f071e25e85ef8e9d12828fe56e5a391f7c4b904
JANG-MINYOUNG <wkdalsdudqq@gmail.com>
2024-01-30 17:44:25 +0900

    ResultData 적용

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.service.MemberService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
+import com.example.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -17,39 +18,39 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		if (Ut.isNullOrEmpty(loginId)) {
-			return "아이디를 입력해주세요";
+			return ResultData.from("F-1", Ut.f("아이디를 입력해주세요."));
 		}
 		if (Ut.isNullOrEmpty(loginPw)) {
-			return "비밀번호를 입력해주세요";
+			return ResultData.from("F-2", Ut.f("비밀번호를 입력해주세요."));
 		}
 		if (Ut.isNullOrEmpty(name)) {
-			return "이름을 입력해주세요";
+			return ResultData.from("F-3", Ut.f("이름을 입력해주세요."));
 		}
 		if (Ut.isNullOrEmpty(nickname)) {
-			return "닉네임을 입력해주세요";
+			return ResultData.from("F-4", Ut.f("닉네임을 입력해주세요."));
 		}
 		if (Ut.isNullOrEmpty(cellphoneNum)) {
-			return "전화번호를 입력해주세요";
+			return ResultData.from("F-5", Ut.f("전화번호를 입력해주세요."));
 		}
 		if (Ut.isNullOrEmpty(email)) {
-			return "이메일을 입력해주세요";
+			return ResultData.from("F-6", Ut.f("이메일을 입력해주세요."));
 		}
 
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 		if (id == -1) {
-			return Ut.f("이미 사용중인 아이디(%s)입니다", loginId);
+			return ResultData.from("F-7", Ut.f("%s은(는) 이미 사용 중 입니다.", loginId));
 		}
 
 		if (id == -2) {
-			return Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email);
+			return ResultData.from("F-8", Ut.f("%s와 %s은(는) 이미 사용 중 입니다.", name, email));
 		}
 
 		Member member = memberService.getMember(id);
 
-		return member;
+		return ResultData.from("S-1", Ut.f("회원가입되었습니다."), member);
 	}
 }
